3cb28992741d1b74a5447df91f889fcfbd9cbf81
ShumCr <152937283+ShumCr@users.noreply.github.com>
2025-09-28 13:13:07 +0200

    Update Scanner.cpp

--- test/liblangutil/Scanner.cpp
+++ test/liblangutil/Scanner.cpp
@@ -13,7 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with solidity.  If not, see <http://www.gnu.org/licenses/>. 
-	1
+	1 2
 */
 /**
  * @author Christian <c@ethdev.com>
