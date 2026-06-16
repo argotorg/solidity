ba7266f8f1b96b029943afdb9fcb89772bfbddea
Manamnath Tiwari <hackathonmanam@gmail.com>
2026-06-16 22:27:31 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,8 @@ html_js_files = ["js/constants.js", "js/initialize.js", "js/toggle.js"]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ["_static/css", "_build/robots.txt"]
+# Change from pointing to the file, to pointing to the target build directory
+html_extra_path = ["_static/css", "_build"] 
 
 # List of templates of static files to be included in the HTML output.
 # Keys represent paths to input files and values are dicts containing:
